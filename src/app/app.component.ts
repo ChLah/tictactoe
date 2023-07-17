52961200c8945db0fc9a9697e736b188efcc9f89
Christoph Lahner <clahner@aptean.com>
2023-07-17 08:29:52 +0200

    Implement translation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'tictactoe';
+  constructor(private translateService: TranslateService) {
+    translateService.setDefaultLang('en');
+    translateService.use('en');
+  }
+
+  curLang$ = this.translateService.onLangChange.pipe(map((e) => e.lang));
+  setLang(lang: string) {
+    this.translateService.use(lang);
+  }
 }
